693d3c89d18e86bbdc3a02aeb98d26302c32d008
Erik Summerfield <esummerfield@phase2technology.com>
2010-12-02 09:26:01 -0600

    added who is command as well as using mongodb

--- plugins/where_is.py
+++ plugins/where_is.py
@@ -4,12 +4,13 @@ import time
 
 class Plugin(plugin.Plugin):
   def load(self):
+    # Define regex as attr of self so we can see which one matched
     self.update_re = re.compile("^[Ii] am (out|in|dnd) ?(.*)$")
     self.where_is_re = re.compile("^[Ww]here is (.*)$")
     self.who_is_re = re.compile("^[Ww]ho is (out|in|dnd)$")
     self.where_is_everyone_re = re.compile("^[Ww]here is everyone$")
-    self.add_watcher(
-      plugin.RegexWatcher(
+    self.add_listener(
+      plugin.RegexListener(
         name="repeat", 
         regexes = [
           self.update_re,
@@ -26,31 +27,53 @@ class Plugin(plugin.Plugin):
       (status, details) = match.groups()
       when = time.localtime()
       user = msg.pam['from'][0]
-      if 'status' not in self.pam.data:
-        self.pam.data['status'] = {}
-      self.pam.data['status'][user] = (status, details, when)
+      doc = {
+        'user' : user,
+        'status' : status,
+        'details' : details,
+        'when' : time.mktime(when)
+      }
+      self.pam.db.where_is.remove("user", user)
+      self.pam.db.where_is.insert(doc)
+
       if status == 'out':
         self.pam.reply_msg(msg, "Enjoy")
       elif status == 'in':
         self.pam.reply_msg(msg, "Back to work")
       elif status == 'dnd':
         self.pam.reply_msg(msg, "No one bug {0}".format(user))
-    if match.re == self.where_is_re:
+    elif match.re == self.where_is_re:
       user = match.group(1)
       print user
-      print self.pam.data
-      if ('status' in self.pam.data) and (user in self.pam.data['status']):
-        (status, details, when) = self.pam.data['status'][user]
-        timestr = self.relative_time(when)
-        body = "{0} is {1} {2} as of {3}".format(user, status, details, timestr)
+      info = self.pam.db.where_is.find_one({"user": user})
+      if info:
+        timestr = self.relative_time(time.localtime(info['when']))
+        body = "{0} is {1} {2} as of {3}".format(user, info['status'], info['details'], timestr)
         self.pam.reply_msg(msg, body)
-    if match.re == self.where_is_everyone_re:
+      else:
+        self.pam.reply_msg(msg, "Sorry I don't know where {0} is.".format(user))
+    elif match.re == self.where_is_everyone_re:
       body = "Status' \n"
-      for (user, (status, details, when)) in self.pam.data['status'].iteritems():
-        timestr = self.relative_time(when)
-        body = "{0}{1} is {2} {3} as of {4}\n".format(body, user, status, details, timestr)
+      for info in self.pam.db.where_is.find():
+        timestr = self.relative_time(time.localtime(info['when']))
+        body = "{0}{1} is {2} {3} as of {4}\n".format(body, info['user'], info['status'], info['details'], timestr)
       self.pam.reply_msg(msg, body)
+    elif match.re == self.who_is_re:
+      status = match.group(1)
+      body = "Currently {0} \n".format(status)
+      for info in self.pam.db.where_is.find({"status":status}):
+        timestr = self.relative_time(time.localtime(info['when']))
+        body = "{0}{1} is {2} {3} as of {4}\n".format(body, info['user'], info['status'], info['details'], timestr)
+      self.pam.reply_msg(msg, body)
+
+
   def relative_time(self, when):
+    """ 
+    take a struct time and compare it with now 
+    if it is the same day give the time, 
+    if the same week give the day of the week
+    else give the time and date
+    """
     now = time.localtime()
     if when[7] == now[7] :
       return time.strftime("%H:%M", when)
